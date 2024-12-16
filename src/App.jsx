119a9d74ed3163752818e7f3dd5bbe0f9e555afc
Danish <ammarsaifi5376@gmail.com>
2024-12-16 16:30:22 +0530

    checking error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Navbar from "./components/navbar";
+// import Navbar from "./components/navbar";
 import LandingPage from "./app/landingPage/LandingPage";
 
 const App = () => {
   return (
     <>
       <div className="min-h-screen bg-white">
-        <Navbar />
+        {/* <Navbar /> */}
         <LandingPage />
       </div>
     </>
